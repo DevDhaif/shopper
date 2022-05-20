8eec03dfa9ab372b99e9f4aa7d235309beba23a8
DevDhaif <devdhaif@gmail.com>
2022-05-20 19:59:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className='flex justify-center'>
+      <h1 className='text-2xl font-bold text-red-500 bg-blue-300'>
+        React and Tailwind with Vitejs!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
